cb56deb7c620c2a5bd9cc4594cc669753aff398a
somnia1337 <2581063732@qq.com>
2025-12-01 15:36:52 +0800

    aoc-2025

---
+++ README.md
@@ -0,0 +1,3 @@
+# [aoc-2025](https://github.com/Somnia1337/aoc-2025)
+
+Somnia1337's solutions to [Advent of Code - 2025](https://adventofcode.com/2025).
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+macro_rules! aoc_days {
+    ($($day:literal => $mod:ident),+ $(,)?) => {
+        $(
+            mod $mod;
+        )+
+
+        fn main() {
+            let day = std::env::args()
+                .nth(1)
+                .expect("Usage: cargo run -- <day>");
+
+            let input_path = match day.as_str() {
+                $(
+                    $day => concat!("input/day", $day),
+                )+
+                _ => panic!("Unknown day: {}", day),
+            };
+
+            let input = std::fs::read_to_string(input_path)
+                .expect("Failed to read input");
+
+            match day.as_str() {
+                $(
+                    $day => $mod::$mod(&input),
+                )+
+                _ => unreachable!(),
+            };
+        }
+    };
+}
